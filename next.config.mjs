f4039bfc0469667fce99b5b908045a06ee8e016b
Moldovan Dennis <dennismoldovan32@gmail.com>
2024-07-23 13:33:23 +0300

    chore - update config

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,19 @@ const nextConfig = {
                 hostname: "lh3.googleusercontent.com",
                 port: "",
                 pathname: "/**",
-            }
+            },
+            {
+                protocol: "https",
+                hostname: "avatars.githubusercontent.com",
+                port: "",
+                pathname: "/**",
+            },
+            {
+                protocol: "https",
+                hostname: "res.cloudinary.com",
+                port: "",
+                pathname: "/*/*/*/*/*",
+              },
         ]
     }
 };
